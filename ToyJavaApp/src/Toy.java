8eb56f3bc7d512715e41d84bbe654e5ba21cc8e7
Vrushali Lahoti <Vrushali.Lahoti@trueposition.com>
2015-05-22 12:02:33 +0530

    first commmit

---
+++ ToyJavaApp/src/Toy.java
@@ -0,0 +1,14 @@
+
+public class Toy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO
+		System.out.println("This is a very small toy");
+		System.out.println("This is a toy");
+		System.out.println("Have a great d");
+	}
+
+}
